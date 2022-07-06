2c40e87969f9e52dc8f6b8af7fb6c6d74647a8dc
暗中观察|.･｀) <2868470542@qq.com>
2022-07-06 22:19:37 +0800

    feat : add user model

--- Summer/Summer/settings.py
+++ Summer/Summer/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'corsheaders',  # 跨域请求
+    'user',
 ]
 
 MIDDLEWARE = [
--- Summer/Summer/urls.py
+++ Summer/Summer/urls.py
@@ -1,4 +1,4 @@
-"""Summer URL Configuration
+"""django_test URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin', admin.site.urls),
+    path('api/user/', include('user.urls')),
+
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
