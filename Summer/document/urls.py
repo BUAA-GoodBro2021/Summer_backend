b5b4d41a214955f4ce9d43a2dd0a11133d007e58
李毅骁 <2947653177@qq.com>
2022-08-01 17:42:18 +0800

    feat: add document route

--- Summer/document/urls.py
+++ Summer/document/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('example', views.example)
+    path('example', views.example),
+    path('create_document', views.create_document),
+    path('list_document_user', views.list_document_user),
+    path('delete_document', views.delete_document),
 ]
