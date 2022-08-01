274e0427eeceebe3e997ff872b3d1f376c71d6fb
李毅骁 <2947653177@qq.com>
2022-08-01 19:28:02 +0800

    feat: link database and websocket

--- Summer/utils/consumers.py
+++ Summer/utils/consumers.py
@@ -2,20 +2,24 @@ from channels.generic.websocket import WebsocketConsumer
 from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
 
+from utils.Redis_utils import *
+
 
 class Consumer(WebsocketConsumer):
-    # 以此来测试共享(后置换成数据库)
-    content = ''
-
     def websocket_connect(self, message):
-        # 握手
-        self.accept()
         # 获取url中的文档id，以此为键
         document_id = self.scope['url_route']['kwargs'].get('document_id')
+        # 获取cache中内容
+        try:
+            document_key, document_dict = cache_get_by_id('document', 'document', document_id)
+        except Exception:
+            return
+        # 握手
+        self.accept()
         # 将客户端添加置对应的document_id的组中
         async_to_sync(self.channel_layer.group_add)(document_id, self.channel_name)
         # 向客户端更新之前写好的内容
-        self.send(self.content)
+        self.send(document_dict['document_content'])
 
     def websocket_receive(self, message):
         # 获取url中的文档id，以此为键
@@ -24,6 +28,14 @@ class Consumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_send)(document_id, {
             'type': 'document.update', 'message': message
         })
+        # 获取cache中内容
+        try:
+            document_key, document_dict = cache_get_by_id('document', 'document', document_id)
+        except Exception:
+            return
+        # 更新cache
+        document_dict['document_content'] = message.get('text')
+        cache.set(document_key, document_dict)
 
     def websocket_disconnect(self, message):
         # 获取url中的文档id，以此为键
@@ -35,5 +47,4 @@ class Consumer(WebsocketConsumer):
     def document_update(self, event):
         # 获取发送信息，更新其他客户端
         text = event.get('message').get('text')
-        self.content = text
         self.send(text)
