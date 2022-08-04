2431e549f5b2ef571f2c824bd85e2f9c4c05707a
暗中观察|.･｀) <2868470542@qq.com>
2022-08-04 10:08:31 +0800

    feat : add renaming page

--- Summer/page/tasks.py
+++ Summer/page/tasks.py
@@ -1,7 +1,6 @@
 from Summer.celery import app
-from team.models import Team
+from page.models import Page
 from project.models import Project
-from user.models import UserToProjectStar
 
 
 @app.task
@@ -9,3 +8,22 @@ def celery_create_page(project_id):
     project = Project.objects.get(id=project_id)
     project.add_file_num()
     return project.to_dic()
+
+
+@app.task
+def celery_save_page(page_id, page_height, page_width, element_list, num):
+    page = Page.objects.get(id=page_id)
+    page.page_height = page_height
+    page.page_width = page_width
+    page.element_list = element_list
+    page.num = num
+    page.save()
+    return page.to_dic()
+
+
+@app.task
+def celery_rename_page(page_id, page_name):
+    page = Page.objects.get(id=page_id)
+    page.page_name = page_name
+    page.save()
+    return page.to_dic()
