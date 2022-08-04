34853453ab63c5da7654b7836c8c9bd3271a403a
暗中观察|.･｀) <2868470542@qq.com>
2022-08-04 11:38:48 +0800

    fix : fix a bug of getting page detail

--- Summer/page/views.py
+++ Summer/page/views.py
@@ -117,7 +117,7 @@ def list_page_detail(request):
     # 获取缓存
     page_key, result = cache_get_by_id_detail('page', 'page', page_id)
     result['element_list'] = result['element_list'].split("|")
-    result['result'] = 0
+    result['result'] = 1
     result['message'] = '成功获取某个页面的详细元素'
 
     return JsonResponse(result)
