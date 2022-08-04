b0bc472b8624bb293b37c46f813ff5d66c92747c
李毅骁 <2947653177@qq.com>
2022-08-04 23:57:36 +0800

    feat: make migrations diagram

--- Summer/diagram/models.py
+++ Summer/diagram/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # 绘图实体
 class Diagram(models.Model):
-    diagram_name = models.CharField('绘图名称', max_length=100, default='')
+    diagram_content = models.TextField('绘图内容', null=True)
 
     # 实体属性
     created_time = models.DateTimeField('创建时间', auto_now_add=True)
@@ -12,7 +12,7 @@ class Diagram(models.Model):
     def to_dic(self):
         return {
             'diagram_id': self.id,
-            'diagram_name': self.diagram_name,
+            'diagram_content': self.diagram_content,
 
             'created_time': self.created_time,
             'updated_time': self.updated_time,
