91dce41bf9152fa26940797bec5b7ec7116fdacf
李毅骁 <2947653177@qq.com>
2022-08-07 16:25:21 +0800

    feat: add cache in consumer

--- Summer/Summer/asgi.py
+++ Summer/Summer/asgi.py
@@ -8,12 +8,13 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from . import routes
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Summer.settings')
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
--- Summer/utils/consumers.py
+++ Summer/utils/consumers.py
@@ -6,7 +6,7 @@ from channels.generic.websocket import WebsocketConsumer
 from channels.exceptions import StopConsumer
 from asgiref.sync import async_to_sync
 
-# from page.tasks import *
+from page.tasks import *
 from utils.Redis_utils import *
 
 
@@ -32,7 +32,6 @@ class Consumer(WebsocketConsumer):
         self.accept()
         # 同步添加组
         async_to_sync(self.channel_layer.group_add)(page_id, self.channel_name)
-        page_dict['element_list'] = page_dict['element_list'].split('|')
         # 向客户端更新之前写好的内容
         self.send(self.json_dumps(page_dict))
 
@@ -51,19 +50,19 @@ class Consumer(WebsocketConsumer):
         page_dict['page_name'] = page_new_dict['page_name']
         page_dict['page_height'] = page_new_dict['page_height']
         page_dict['page_width'] = page_new_dict['page_width']
-        page_dict['element_list'] = page_new_dict['element_list'].split('|')
+        page_dict['element_list'] = page_new_dict['element_list']
         page_dict['num'] = page_new_dict['num']
 
         cache.set(page_key, page_dict)
-        # # 异步更新数据库
-        # celery_save_page.delay(
-        #     page_id,
-        #     page_dict['page_name'],
-        #     page_dict['page_height'],
-        #     page_dict['page_width'],
-        #     page_dict['element_list'],
-        #     page_dict['num']
-        # )
+        # 异步更新数据库
+        celery_save_page.delay(
+            page_id,
+            page_dict['page_name'],
+            page_dict['page_height'],
+            page_dict['page_width'],
+            page_dict['element_list'],
+            page_dict['num']
+        )
 
     def websocket_disconnect(self, message):
         # 获取url中的文档id，以此为键
