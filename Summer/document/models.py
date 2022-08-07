879f08f0f38f83cf13378ddf5db2136d130608f4
暗中观察|.･｀) <2868470542@qq.com>
2022-08-07 23:16:38 +0800

    feat : make a big reconsitution of document

--- Summer/document/models.py
+++ Summer/document/models.py
@@ -6,7 +6,7 @@ from utils.Redis_utils import cache_get_by_id
 
 class Document(models.Model):
     creator_id = models.IntegerField('创建者id', default=0)
-    project_id = models.IntegerField('项目id', default=0)
+    team_id = models.IntegerField('团队id', default=0)
     creator_name = models.CharField('创建者名称', max_length=100, default='')
     document_title = models.CharField('文档标题', max_length=100, default='')
     document_content = models.TextField('文档内容', default='')
@@ -28,8 +28,8 @@ class Document(models.Model):
         return {
             'document_id': self.id,
             'creator_id': self.creator_id,
+            'team_id': self.team_id,
             'creator_name': self.creator_name,
-            'project_id': self.project_id,
 
             'document_title': self.document_title,
             'document_content': self.document_content,
@@ -58,7 +58,7 @@ def recurse_display(data):
     return display_list
 
 
-def recurse_display_copy(creator_id, project_id, parent_id, old_document_query_set):
+def recurse_display_copy(creator_id, old_document_query_set, parent_id, new_folder_id):
     """递归复制树"""
     # 创建新项目与新文件之间的关系
     for every_old_document_query_set in old_document_query_set:
@@ -71,20 +71,19 @@ def recurse_display_copy(creator_id, project_id, parent_id, old_document_query_s
                                                document_title=old_document_dict['document_title'],
                                                document_content=old_document_dict['document_content'],
                                                is_folder_or_file=old_document_dict['is_folder_or_file'],
-                                               project_id=project_id)
-        if parent_id == 0:
-            new_document.parent = None
+                                               )
+
+        # 第一层项目项目的父亲节点需要更新修正
+        if new_folder_id != 0:
+            new_document.parent_id = new_folder_id
         else:
             new_document.parent_id = parent_id
         new_document.save()
 
-        # 创建关系
-        ProjectToDocument.objects.create(project_id=project_id, document_id=new_document.id)
-
         # 孩子信息
         children = every_old_document_query_set.children.all()
         if len(children) > 0:
-            recurse_display_copy(creator_id, project_id, new_document.id, children)
+            recurse_display_copy(creator_id, children, new_document.id, 0)
 
 
 def recurse_display_id(data):
@@ -101,10 +100,4 @@ def recurse_display_id(data):
 # 用户与文档关联表
 class UserToDocument(models.Model):
     user_id = models.IntegerField('用户id', default=0)
-    document_id = models.IntegerField('文档id', default=0)
-
-
-# 项目与文档关联表
-class ProjectToDocument(models.Model):
-    project_id = models.IntegerField('项目id', default=0)
     document_id = models.IntegerField('文档id', default=0)
--- Summer/project/urls.py
+++ Summer/project/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('rename_project', views.rename_project),
     path('delete_project', views.delete_project),
     path('copy_project', views.copy_project),
-    path('copy_project_tree_document', views.copy_project_tree_document),
     path('remove_project_to_bin', views.remove_project_to_bin),
     path('recover_project_from_bin', views.recover_project_from_bin),
     path('add_star_project', views.add_star_project),
