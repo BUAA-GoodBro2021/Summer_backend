15fc00dd1a969f0fd4205de89a98e75b07809fd6
李毅骁 <2947653177@qq.com>
2022-08-08 15:12:20 +0800

    feat: add change is_preview

--- Summer/page/tasks.py
+++ Summer/page/tasks.py
@@ -26,7 +26,7 @@ def celery_rename_page(page_id, page_name):
 @app.task
 def celery_change_preview(page_id, is_preview):
     page = Page.objects.get(id=page_id)
-    page.is_preview = is_preview
+    page.is_preview = int(is_preview)
     page.save()
     return page.to_dic()
 
