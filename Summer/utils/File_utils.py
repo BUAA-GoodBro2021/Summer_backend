89d64a577fdbae46239b69c6baab4b510f2fcf1b
暗中观察|.･｀) <2868470542@qq.com>
2022-08-09 21:39:13 +0800

    fix: check model route

--- Summer/utils/File_utils.py
+++ Summer/utils/File_utils.py
@@ -105,7 +105,7 @@ def upload_image(image, bucket_name, model_id, is_audit=False, image_size=1024 *
 
 # 获取文件内容
 def read_file(model_type):
-    f = open(os.getcwd() + '/document_models/model' + str(model_type) + '.txt', encoding="utf-8")
+    f = open(os.getcwd() + '/document_models/model' + str(model_type) + '.txt', encoding="utf-8mb4")
     txt = f.read()
     f.close()
     return txt
