adfd07c66a37ea2dc3569bb3ef5998e12de99ee2
暗中观察|.･｀) <2868470542@qq.com>
2022-08-11 16:03:42 +0800

    feat : start registering

--- Summer/user/views.py
+++ Summer/user/views.py
@@ -18,54 +18,54 @@ def register(request):
 
     请求体包含包含 username，password1，password2，email
     """
-    return JsonResponse({'result': 0, 'message': '暂时不开放注册哟~'})
+    # return JsonResponse({'result': 0, 'message': '暂时不开放注册哟~'})
 
-    # if request.method == 'POST':
-    #
-    #     # 获取表单信息
-    #     username = request.POST.get('username', '')
-    #     real_name = request.POST.get('real_name', '')
-    #     password1 = request.POST.get('password1', '')
-    #     password2 = request.POST.get('password2', '')
-    #
-    #     if len(username) == 0 or len(real_name) == 0 or len(password1) == 0 or len(password2) == 0:
-    #         result = {'result': 0, 'message': r'用户名, 真实姓名与密码不允许为空!'}
-    #         return JsonResponse(result)
-    #
-    #     if User.objects.filter(username=username, is_active=True).exists():
-    #         result = {'result': 0, 'message': r'用户已存在!'}
-    #         return JsonResponse(result)
-    #
-    #     if password1 != password2:
-    #         result = {'result': 0, 'message': r'两次密码不一致!'}
-    #         return JsonResponse(result)
-    #
-    #     email = request.POST.get('email', '')
-    #
-    #     if len(email) == 0:
-    #         result = {'result': 0, 'message': r'邮箱不允许为空!'}
-    #         return JsonResponse(result)
-    #
-    #     # 生成假用户
-    #     user = User.objects.create(username=username, real_name=real_name, password=hash_encode(password1),
-    #                                is_active=False)
-    #
-    #     # 需要加密的信息
-    #     payload = {
-    #         'user_id': user.id,
-    #         'email': email,
-    #     }
-    #     # 发送注册邮件
-    #     send_result = send_email(payload, email, 'register')
-    #     if not send_result:
-    #         result = {'result': 0, 'message': r'发送失败!请检查邮箱格式'}
-    #         return JsonResponse(result)
-    #     else:
-    #         result = {'result': 1, 'message': r'发送成功!请及时在邮箱中查收.'}
-    #         return JsonResponse(result)
-    # else:
-    #     result = {'result': 0, 'message': r"请求方式错误！"}
-    #     return JsonResponse(result)
+    if request.method == 'POST':
+
+        # 获取表单信息
+        username = request.POST.get('username', '')
+        real_name = request.POST.get('real_name', '')
+        password1 = request.POST.get('password1', '')
+        password2 = request.POST.get('password2', '')
+
+        if len(username) == 0 or len(real_name) == 0 or len(password1) == 0 or len(password2) == 0:
+            result = {'result': 0, 'message': r'用户名, 真实姓名与密码不允许为空!'}
+            return JsonResponse(result)
+
+        if User.objects.filter(username=username, is_active=True).exists():
+            result = {'result': 0, 'message': r'用户已存在!'}
+            return JsonResponse(result)
+
+        if password1 != password2:
+            result = {'result': 0, 'message': r'两次密码不一致!'}
+            return JsonResponse(result)
+
+        email = request.POST.get('email', '')
+
+        if len(email) == 0:
+            result = {'result': 0, 'message': r'邮箱不允许为空!'}
+            return JsonResponse(result)
+
+        # 生成假用户
+        user = User.objects.create(username=username, real_name=real_name, password=hash_encode(password1),
+                                   is_active=False)
+
+        # 需要加密的信息
+        payload = {
+            'user_id': user.id,
+            'email': email,
+        }
+        # 发送注册邮件
+        send_result = send_email(payload, email, 'register')
+        if not send_result:
+            result = {'result': 0, 'message': r'发送失败!请检查邮箱格式'}
+            return JsonResponse(result)
+        else:
+            result = {'result': 1, 'message': r'发送成功!请及时在邮箱中查收.'}
+            return JsonResponse(result)
+    else:
+        result = {'result': 0, 'message': r"请求方式错误！"}
+        return JsonResponse(result)
 
 
 # 用户注册
