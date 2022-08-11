24e6457a14ee398d4a23d7c2e8ccfd3dede27433
李毅骁 <2947653177@qq.com>
2022-08-11 09:53:57 +0800

    fix: check diagram content

--- Summer/diagram/views.py
+++ Summer/diagram/views.py
@@ -31,8 +31,7 @@ def create_token(request):
     diagram_token = sign_token({
         'project_id': int(project_id),
         'diagram_id': diagram.id,
-        'diagram_name': diagram.diagram_name,
-        'diagram_content': diagram.diagram_content
+        'diagram_name': diagram.diagram_name
     })
     result = {'result': 1, 'message': '获取绘图token成功!', 'diagram_token': diagram_token}
     return JsonResponse(result)
